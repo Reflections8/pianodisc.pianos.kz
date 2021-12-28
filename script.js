6d47a31876d7ee7b3e8c04618e22b42c19d57c25
devart <primary.devart@gmail.com>
2021-12-29 00:58:33 +0600

    added phones mask

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-'use strict'
-
 document.addEventListener('DOMContentLoaded', function() {
 
 let burgerIcon = document.querySelector('.burger_menu'),
@@ -66,12 +64,14 @@ popupClose.addEventListener('click', function() {
 	popupBgForm.classList.add('hide');
 });
 
-formBtn.addEventListener('click', function () {
+//popup for main form
+
+/* formBtn.addEventListener('click', function () {
 	popupSuccess.classList.remove('hide');
 	setTimeout(function(){ 
 		popupSuccess.classList.add('hide');
 		}, 3000);
-});
+}); */
 
 formBtnPopUp.addEventListener('click', function () {
 	popupBgForm.classList.add('hide');
@@ -80,5 +80,88 @@ formBtnPopUp.addEventListener('click', function () {
 		popupSuccess.classList.add('hide');
 		}, 3000);
 });
+
+/*PHONE MASK*/
+let phoneInputs = document.querySelectorAll('input[data-tel-input]');
+let phoneInputsPop = popupBgForm.querySelectorAll('input[data-tel-input]');
+console.log(phoneInputs);
+
+
+let getInputNumbersValue = function(input) {
+	return input.value.replace(/\D/g, "");
+}
+
+let onPhoneInput = function(e) {
+	let input = e.target,
+		 inputNumbersValue = getInputNumbersValue(input);
+		 formattedInputValue = "",
+		 selectionStart = input.selectionStart;
+
+	if (!inputNumbersValue) {
+		return input.value = "";
+	}
+
+	if (input.value.length != selectionStart) {
+		if (e.data && /\D/g.test(e.data)) {
+			input.value = inputNumbersValue;
+		}
+
+		return;
+	}
+
+	if (["7", "8"].indexOf(inputNumbersValue[0]) > -1) {
+		//KZ Number
+		if (inputNumbersValue[0] == "7", "8") {
+			let firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+			formattedInputValue = firstSymbols + " ";
+			if (inputNumbersValue.length > 1) {
+				formattedInputValue += "" + inputNumbersValue.substring(1, 4);
+			}
+			if (inputNumbersValue >= 5) {
+				formattedInputValue += " " + inputNumbersValue.substring(4, 7);
+			}
+			if (inputNumbersValue >= 7) {
+				formattedInputValue += " " + inputNumbersValue.substring(7, 9);
+			}
+			if (inputNumbersValue >= 9) {
+				formattedInputValue += " " + inputNumbersValue.substring(9, 11);
+			}
+		}
+	} else {
+		//Not KZ number
+		formattedInputValue = "+" + inputNumbersValue;
+	}
+	input.value = formattedInputValue;
+}
+
+let onPhoneKeyDown = function(e) {
+	let input = e.target;
+	if (e.keyCode == 8 && getInputNumbersValue(input).length > 1) {
+		input.value = getInputNumbersValue(input);
+	} else if (e.keyCode == 8 && getInputNumbersValue(input).length == 1) {
+		input.value = "";
+	}
+}
+
+let onPhonePaste = function(e) {
+	let pasted = e.clipboardData || window.clipboardData,
+		input = e.target,
+		inputNumbersValue = getInputNumbersValue(input);
+
+		if (pasted) {
+			let pastedText = pasted.getData("Text");
+			if (/\D/g.test(pastedText)) {
+				input.value = inputNumbersValue;
+			}
+		}
+}
+
+
+for (i = 0; i < phoneInputs.length; i++) {
+	let input = phoneInputs[i];
+	input.addEventListener('input', onPhoneInput);
+	input.addEventListener('keydown', onPhoneKeyDown);
+	input.addEventListener('paste', onPhonePaste);
+}
 
 });
